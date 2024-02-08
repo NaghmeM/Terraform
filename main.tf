419e396814955129c664a070a4bf498edb5ab05f
Najmeh M <eostremola@gmail.com>
2024-02-08 12:39:21 +0200

    add first vpc

--- main.tf
+++ main.tf
@@ -1,8 +1,25 @@
-resource "aws_instance" "testweb" {
-  ami           = "ami-0014ce3e52359afbd"
-  instance_type = "t3.micro"
+# resource "aws_instance" "testweb" {
+#   ami           = "ami-0014ce3e52359afbd"
+#   instance_type = "t3.micro"
+
+#   tags = {
+#     Created_by = "terraform"
+#   }
+# }
+
+resource "aws_vpc" "IT-INFRA" {
+  
+  cidr_block = "172.16.0.0/16"
 
   tags = {
-    Created_by = "terraform"
+    Name = "IT Infra"
+  }
+}
+resource "aws_subnet" "VAS_subnet" {
+  vpc_id     = aws_vpc.IT-INFRA.id
+  cidr_block = "172.16.10.0/24"
+
+  tags = {
+    Name = "VAS"
   }
 }
